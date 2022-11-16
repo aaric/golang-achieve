066a4969ab0327dcb3a204eaf6dbf22b4b7b7aaa
qlhan <qlhan@incarcloud.com>
2022-11-16 17:14:30 +0800

    feat: canvas demo
    draw rect

--- fyne.go
+++ fyne.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
+	"image/color"
 	"time"
 )
 
@@ -34,11 +36,33 @@ func main() {
 	}()*/
 
 	// open window
-	myWindow.SetContent(widget.NewButton("Open Window", func() {
+	/*myWindow.SetContent(widget.NewButton("Open Window", func() {
 		w2 := myApp.NewWindow("Window 2")
 		w2.SetContent(widget.NewLabel("This is other windows."))
 		w2.Show()
-	}))
+	}))*/
+
+	// canvas
+	myCanvas := myWindow.Canvas()
+	blue := color.NRGBA{
+		R: 0,
+		G: 0,
+		B: 180,
+		A: 255,
+	}
+	rect := canvas.NewRectangle(blue)
+	myCanvas.SetContent(rect)
+	go func() {
+		time.Sleep(time.Second)
+		green := color.NRGBA{
+			R: 0,
+			G: 180,
+			B: 0,
+			A: 255,
+		}
+		rect.FillColor = green
+		rect.Refresh()
+	}()
 
 	//myWindow.ShowAndRun()
 	myWindow.Show()
